ee15861da1fb3c1246c44a315e4f93a1855a673e
Ganesh R <ganesramesh110@gmail.com>
2020-02-23 01:29:03 +0530

    feat: Change profile picture and site title

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ const SEO = ({ description, lang, meta, title }) => {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate={`%s - A blog by Ganesh R`}
       meta={[
         {
           name: `description`,
