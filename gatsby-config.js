3835f246849251273353cfba03552c8824db16c9
Ganesh R <ganesramesh110@gmail.com>
2020-03-01 23:29:27 +0530

    fix: gatsby build failing

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/profile-pic.png`,
+        icon: `content/assets/profile-pic.jpg`,
       },
     },
     `gatsby-plugin-react-helmet`,
